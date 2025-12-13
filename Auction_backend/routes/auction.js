dd944f29c3fb80a82a88716758504d615d6885d2
JissJanex <jissjanex@gmail.com>
2025-12-13 23:59:19 +0300

    test

--- Auction_backend/routes/auction.js
+++ Auction_backend/routes/auction.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Get all active or upcoming auctions
 router.get("/", async (req, res) => {
   try {
-    console.log("db");
+    console.log(db);
     const result = await db.query(`
       SELECT * FROM auctions
       WHERE end_time > NOW()

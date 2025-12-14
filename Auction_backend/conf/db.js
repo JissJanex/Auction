92d294f132566d7ca407eef1b04244669dc472b8
JissJanex <jissjanex@gmail.com>
2025-12-14 15:03:23 +0300

    added family to db query

--- Auction_backend/conf/db.js
+++ Auction_backend/conf/db.js
@@ -4,13 +4,12 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-db.query("SELECT 1")
-  .then(() => console.log("DB connected"))
-  .catch(err => console.error("DB connection error:", err));
-
-
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   family: 4,
 });
+
+db.query("SELECT 1")
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error("DB connection error:", err));
